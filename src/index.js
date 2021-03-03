8cd734b7d982dab85023c73b08bf384614730933
ngirimana <chadrackngirimana@gmail.com>
2021-03-03 22:48:07 +0200

    authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 import Login from './components/Auth/Login';
 import Register from "./components/Auth/Register";
 import reportWebVitals from "./reportWebVitals";
+import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Root = () => (
@@ -18,9 +18,9 @@ const Root = () => (
 );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
+  
+    <Root />,
+ 
   document.getElementById("root")
 );
 
