823a05536a4927faae6bcc1d5ca00108d327fc61
ngirimana <chadrackngirimana@gmail.com>
2021-03-04 23:59:48 +0200

    public channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,7 +1,8 @@
 import React from "react";
 import UserPanel from "./UserPanel";
 import { Menu } from "semantic-ui-react";
-
+import Channels from './Channels';
+  
 class SidePanel extends React.Component {
   render() {
     const { currentUser } = this.props;
@@ -15,6 +16,7 @@ class SidePanel extends React.Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
+        <Channels currentUser={currentUser}/>
       </Menu>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,47 @@
-import { combineReducers} from 'redux';
+import { combineReducers } from "redux";
 import * as actionTypes from "../actions/types";
 
-const initialState = {
-    currentUser: null,
-    isLoading:true
-}
-const userReducer = (state=initialState, action) => {
-    switch (action.type) {
-        case actionTypes.SET_USER:
-            return {
-                currentUser: action.payload.currentUser,
-                isLoading: false
-            };
-        case actionTypes.CLEAR_USER:
-            return {
-                ...initialState,
-                isLoading:false 
-        }
-        
-        default:
-            return state
-    }
-}
+const initialUserState = {
+  currentUser: null,
+  isLoading: true,
+};
+
+const user_reducer = (state = initialUserState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        currentUser: action.payload.currentUser,
+        isLoading: false,
+      };
+    case actionTypes.CLEAR_USER:
+      return {
+        ...state,
+        isLoading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialChannelState = {
+  currentChannel: null,
+};
+
+const channel_reducer = (state = initialChannelState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_CURRENT_CHANNEL:
+      return {
+        ...state,
+        currentChannel: action.payload.currentChannel,
+      };
+    default:
+      return state;
+  }
+};
 
 const rootReducer = combineReducers({
-    user:userReducer
-})
-export default rootReducer
+  user: user_reducer,
+  channel: channel_reducer,
+});
+
+export default rootReducer;
