c8a2f80b5bc09d1a3860199595052ee679f6e3b5
ngirimana <chadrackngirimana@gmail.com>
2021-03-07 22:39:31 +0200

    sending media file

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,8 @@ class Messages extends React.Component {
     messages: [],
     messagesLoading: true,
     channel: this.props.currentChannel,
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    progressBar:false
   };
 
   componentDidMount() {
@@ -48,16 +49,20 @@ class Messages extends React.Component {
         user={this.state.user}
       />
     ));
-
+  isProgressBarVisible = percent => {
+    if (percent > 0) {
+    this.setState({progressBar:true})
+  }
+}
   render() {
-    const { messagesRef, messages, channel, user } = this.state;
+    const { messagesRef, messages, channel, user,progressBar } = this.state;
 
     return (
       <React.Fragment>
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className="messages">
+          <Comment.Group className={progressBar?'.messages__progress':"messages"}>
             {this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
@@ -66,6 +71,7 @@ class Messages extends React.Component {
           messagesRef={messagesRef}
           currentChannel={channel}
           currentUser={user}
+          isProgressBarVisible={this.isProgressBarVisible()}
         />
       </React.Fragment>
     );
