2795b8ca85930a741225eea254060a49c18159dc
ngirimana <chadrackngirimana@gmail.com>
2021-03-09 21:45:47 +0200

    add starred channels

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -3,6 +3,8 @@ import UserPanel from "./UserPanel";
 import Channels from "./Channels";
 import { Menu } from "semantic-ui-react";
 import DirectMessages from './DirectMessages';
+import Starred from './Starred';
+
 class SidePanel extends React.Component {
   render() {
     const { currentUser } = this.props;
@@ -16,6 +18,7 @@ class SidePanel extends React.Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
+        <Starred currentUser={ currentUser}/>
         <Channels currentUser={currentUser} />
         <DirectMessages currentUser={currentUser} />
       </Menu>
