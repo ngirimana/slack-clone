7bd387febc0a713f03573c1b1bdef1c600f44605
ngirimana <chadrackngirimana@gmail.com>
2021-03-10 09:26:04 +0200

    choose color panel

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,44 +1,56 @@
 import * as actionTypes from "./types";
 
 /* User Actions */
-export const setUser = user => {
+export const setUser = (user) => {
   return {
     type: actionTypes.SET_USER,
     payload: {
-      currentUser: user
-    }
+      currentUser: user,
+    },
   };
 };
 
 export const clearUser = () => {
   return {
-    type: actionTypes.CLEAR_USER
+    type: actionTypes.CLEAR_USER,
   };
 };
 
 /* Channel Actions */
-export const setCurrentChannel = channel => {
+export const setCurrentChannel = (channel) => {
   return {
     type: actionTypes.SET_CURRENT_CHANNEL,
     payload: {
-      currentChannel: channel
-    }
-  }
-}
+      currentChannel: channel,
+    },
+  };
+};
 
-export const setPrivateChannel = isPrivateChannel => {
+export const setPrivateChannel = (isPrivateChannel) => {
   return {
     type: actionTypes.SET_PRIVATE_CHANNEL,
     payload: {
-      isPrivateChannel
-    }
-  }
-}
+      isPrivateChannel,
+    },
+  };
+};
+
 export const setUserPosts = (userPosts) => {
   return {
     type: actionTypes.SET_USER_POSTS,
     payload: {
-      userPosts
-    }
-  }
-}
+      userPosts,
+    },
+  };
+};
+
+/* Colors Actions */
+export const setColors = (primaryColor, secondaryColor) => {
+  return {
+    type: actionTypes.SET_COLORS,
+    payload: {
+      primaryColor,
+      secondaryColor,
+    },
+  };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import * as actionTypes from "../actions/types";
 
 const initialUserState = {
   currentUser: null,
-  isLoading: true
+  isLoading: true,
 };
 
 const user_reducer = (state = initialUserState, action) => {
@@ -11,12 +11,12 @@ const user_reducer = (state = initialUserState, action) => {
     case actionTypes.SET_USER:
       return {
         currentUser: action.payload.currentUser,
-        isLoading: false
+        isLoading: false,
       };
     case actionTypes.CLEAR_USER:
       return {
         ...state,
-        isLoading: false
+        isLoading: false,
       };
     default:
       return state;
@@ -26,7 +26,7 @@ const user_reducer = (state = initialUserState, action) => {
 const initialChannelState = {
   currentChannel: null,
   isPrivateChannel: false,
-  userPosts: null
+  userPosts: null,
 };
 
 const channel_reducer = (state = initialChannelState, action) => {
@@ -34,27 +34,44 @@ const channel_reducer = (state = initialChannelState, action) => {
     case actionTypes.SET_CURRENT_CHANNEL:
       return {
         ...state,
-        currentChannel: action.payload.currentChannel
-      }
-    case actionTypes.SET_PRIVATE_CHANNEL: {
+        currentChannel: action.payload.currentChannel,
+      };
+    case actionTypes.SET_PRIVATE_CHANNEL:
       return {
         ...state,
-        isPrivateChannel:action.payload.isPrivateChannel
-      }
-    }
+        isPrivateChannel: action.payload.isPrivateChannel,
+      };
     case actionTypes.SET_USER_POSTS:
       return {
         ...state,
-        userPosts:action.payload.userPosts,
-      }
+        userPosts: action.payload.userPosts,
+      };
     default:
       return state;
   }
-}
+};
+
+const initialColorsState = {
+  primaryColor: "#4c3c4c",
+  secondaryColor: "#eee",
+};
+
+const colors_reducer = (state = initialColorsState, action) => {
+  switch (action.type) {
+    case actionTypes.SET_COLORS:
+      return {
+        primaryColor: action.payload.primaryColor,
+        secondaryColor: action.payload.secondaryColor,
+      };
+    default:
+      return state;
+  }
+};
 
 const rootReducer = combineReducers({
   user: user_reducer,
-  channel: channel_reducer
+  channel: channel_reducer,
+  colors: colors_reducer,
 });
 
 export default rootReducer;
